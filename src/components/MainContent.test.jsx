b8159c738c982037c71f50bccda67afbb0498120
Sirui Zhang <siruizhangdev@gmail.com>
2022-12-11 07:06:43 -0500

    sabotage the test file for MainContent to produce failed test result

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -19,6 +19,7 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('help-area')).toBeInTheDocument();
+    expect(screen.getByTestId('help-are')).toBeInTheDocument();
+    // sabotage the test: help-area -> help-are
   });
 });
